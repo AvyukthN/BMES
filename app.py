c8cda555bdaf54092ebc4e3d4d5bcad1a6e1b475
Avyukth Nilajagi <73862038+AvyukthN@users.noreply.github.com>
2021-05-26 00:20:15 -0400

    updated functionality on site

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import smtplib
 
 app = Flask(__name__)
 
@@ -8,7 +9,6 @@ def emailer(name, subject, body):
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'GET':
-        context = {'name': 'ur mom'}
         return render_template('home.html')
 
     if request.method == 'POST':
@@ -40,6 +40,34 @@ def addPost():
 
         print(title, body, projName)
         return render_template('addPost.html')
+
+@app.route('/question', methods = ['GET', 'POST'])
+def question():
+    if request.method == 'GET':
+        return render_template('question.html')
+
+    if request.method == 'POST':
+        if request.form['subject'] and request.form['body']:
+            subject = request.form['subject']
+            body = request.form['body']
+
+            server = smtplib.SMTP('smtp.gmail.com', 587)
+            server.ehlo()
+            server.starttls()
+            server.ehlo()
+
+            server.login("avyukthnilajagi@gmail.com", "Night04Monkey$")
+
+            msg = f"Subject: {subject}\n\n{body}"
+
+            sender = "avyukthnilajagi@gmail.com"
+            recipients = ["avyukthnilajagi@gmail.com", "tanishka.mehta06@gmail.com"]
+           
+            for i in range(len(recipients)):
+                server.sendmail(sender, recipients[i], msg)
+        
+        return render_template('question.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
